497b47f222a7a6e56a75279ed488d94ddad7a8ad
Ismael Sebastián Andrango Peralbo <51093008+SebastianAndrango@users.noreply.github.com>
2023-01-17 11:03:38 -0500

    Resolve comments

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,6 @@ import TextFieldAtom from "./components/atoms/TextFieldAtom"
 function App() {
   return (
     <>
-      <TextFieldAtom 
-        color={""}
-        id={"standard-basic"}
-        label={"Example Text"}
-        type={"text"}
-        placeholder={"Example Text"} 
-        variant={"standard"}      ></TextFieldAtom>
     </>
     
   )
