b881331f5580ab5270b17fcf8eccbf75c51f2c34
Damoz1606 <panchitodmun@gmail.com>
2023-01-17 18:34:52 -0500

    AF-41:Solve button icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,6 @@
-import { Code } from "@mui/icons-material";
-import ButtonIcon from "./components/atoms/ButtonIcon";
-import { ColorPalette } from "./style/ColorPalette";
-import { Shape } from "./style/Shape";
-
 function App() {
-  return (
-    <>
-      <ButtonIcon
-        icon={Code}
-        style={{
-          backgroundColor: ColorPalette.PRIMARY,
-          maxSize: "50px"
-        }}
-        onClick={() => console.log("Works!")}
-      />
-      <ButtonIcon
-        icon={Code}
-        style={{
-          backgroundColor: ColorPalette.SECONDARY,
-          maxSize: "50px"
-        }}
-        onClick={() => console.log("Works!")}
-      />
-      <ButtonIcon
-        icon={Code}
-        style={{
-          backgroundColor: ColorPalette.TERNARY,
-          maxSize: "50px"
-        }}
-        onClick={() => console.log("Works!")}
-      />
-      <ButtonIcon
-        icon={Code}
-        style={{
-          backgroundColor: ColorPalette.FOURTH,
-          color: ColorPalette.SECONDARY,
-          maxSize: "50px"
-        }}
-        onClick={() => console.log("Works!")}
-      />
-    </>
-  )
+
+  return (<></>)
 }
 
 export default App
--- src/components/atoms/ButtonIcon.tsx
+++ src/components/atoms/ButtonIcon.tsx
@@ -1,85 +1,35 @@
 import React from 'react'
-import { ColorPalette } from '../../style/ColorPalette';
-import { Shape } from '../../style/Shape';
-
-const style: React.CSSProperties = {
-    margin: "10px",
-    padding: "10px",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-}
+import { IconButton, styled } from '@mui/material';
 
 interface Props {
+    color: string,
     icon: any,
-    style: {
-        backgroundColor: ColorPalette,
-        size?: number | string,
-        maxSize?: number | string,
-        color?: ColorPalette
-    }
-    onClick: () => void;
-    shape?: Shape,
-    isFloating?: boolean,
-    top?: boolean
-    bottom?: boolean,
-    left?: boolean,
-    right?: boolean,
+    onClick: () => void,
+    size?: number | string
 }
+
+const CustomButton = styled(IconButton)`
+&{
+    padding: 0.5rem;
+}
+& > svg{
+    width: 100%;
+    height: 100%;
+}
+`;
 
 const ButtonIcon = (props: Props) => {
 
-    const createStyle = (): React.CSSProperties => {
-
-        if (!props.shape) {
-            if (props.shape == Shape.SQUARE) {
-                return {
-                    backgroundColor: props.style.backgroundColor,
-                    width: props.style.size ? props.style.size : "100%",
-                    height: props.style.size ? props.style.size : "100%",
-                    maxWidth: props.style.maxSize ? props.style.maxSize : "100px",
-                    maxHeight: props.style.maxSize ? props.style.maxSize : "100px",
-                    color: props.style.color ? props.style.color : ColorPalette.ACCENT,
-                    ...style
-                };
-            }
-        }
-        return {
-            borderRadius: "100%",
-            backgroundColor: props.style.backgroundColor,
-            width: props.style.size ? props.style.size : "100%",
-            height: props.style.size ? props.style.size : "100%",
-            maxWidth: props.style.maxSize ? props.style.maxSize : "100px",
-            maxHeight: props.style.maxSize ? props.style.maxSize : "100px",
-            color: props.style.color ? props.style.color : ColorPalette.ACCENT,
-            ...style
-        };
-    }
-
-    const floatingStyle = () => {
-        let style = {};
-        if(!!props.isFloating && !!props.top){
-            style = {...style, top: 0};
-        } else if(!!props.isFloating && !!props.bottom){
-            style = {...style, bottom: 0};
-        }
-
-        if(!!props.isFloating && !!props.left){
-            style = {...style, left: 0};
-        } else if(!!props.isFloating && !!props.right){
-            style = {...style, right: 0};
-        }
-        return style;
-    }
-
     return (
         <>
-            <button
-                className={`button ${props.isFloating ? 'float' : ''}`}
-                onClick={props.onClick}
-                style={{ ...floatingStyle(), ...createStyle() }}>
-                <props.icon style={{ width: "100%", height: "100%", maxWidth: "100px", maxHeight: "100px" }} />
-            </button>
+            <CustomButton
+                sx={{
+                    color: props.color,
+                    width: props.size ? props.size : '50px',
+                    height: props.size ? props.size : '50px',
+                }}>
+                {props.icon}
+            </CustomButton>
         </>
     )
 }
