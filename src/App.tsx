db259571a8c090739d44d14d93530b8e55266c19
Damoz1606 <panchitodmun@gmail.com>
2023-01-17 18:41:45 -0500

    AF-41: Floating function

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { Code, Abc } from "@mui/icons-material";
+import ButtonIcon from "./components/atoms/ButtonIcon";
+import { ColorPalette } from "./style/ColorPalette";
+import { Shape } from "./style/Shape";
+
 function App() {
 
-  return (<></>)
+  return (
+    <>
+    </>
+  )
 }
 
 export default App
--- src/components/atoms/ButtonIcon.tsx
+++ src/components/atoms/ButtonIcon.tsx
@@ -5,11 +5,20 @@ interface Props {
     color: string,
     icon: any,
     onClick: () => void,
+    float?: boolean,
+    top?: boolean,
+    bottom?: boolean,
+    left?: boolean,
+    right?: boolean,
     size?: number | string
 }
 
 const CustomButton = styled(IconButton)`
 &{
+    padding: 0.5rem;
+    margin: 0.5rem;
+}
+&.float{
     padding: 0.5rem;
 }
 & > svg{
@@ -24,6 +33,11 @@ const ButtonIcon = (props: Props) => {
         <>
             <CustomButton
                 sx={{
+                    position: props.float ? 'absolute' : 'relative',
+                    top: props.top ? 0 : 'auto',
+                    bottom: props.bottom ? 0 : 'auto',
+                    left: props.left ? 0 : 'auto',
+                    right: props.right ? 0 : 'auto',
                     color: props.color,
                     width: props.size ? props.size : '50px',
                     height: props.size ? props.size : '50px',
