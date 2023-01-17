add1ac5f7a03b5711bcb4657fb22072669c5926b
listerineh <jsalvarez4@espe.edu.ec>
2023-01-17 17:48:35 -0500

    feat: include example

--- src/components/molecules/TableMolecule.tsx
+++ src/components/molecules/TableMolecule.tsx
@@ -1,6 +1,34 @@
 import { Table, TableBody, TableContainer, TableHead, TableRow } from '@mui/material'
 import TableCellAtom from '../atoms/TableCellAtom';
 
+/*
+Example of use:
+
+  const headersMock = [
+    'header 1',
+    'header 2',
+    'header 3',
+    'header 4'
+  ]
+
+  const rowsMock = [
+    [
+      'row 1',
+      'row 2',
+      'row 3',
+      'row 4'
+    ],
+    [
+      'row 5',
+      'row 6',
+      'row 7',
+      'row 8'
+    ]
+  ]
+
+  <TableMolecule headers={headersMock} rows={rowsMock} />
+
+*/
 
 interface TableProps {
   headers: string[],
