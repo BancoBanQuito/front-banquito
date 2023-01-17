5bd9482bd5f1777cd4b62da3906c3e40c778169b
Ismael Sebastián Andrango Peralbo <51093008+SebastianAndrango@users.noreply.github.com>
2023-01-17 10:32:39 -0500

    AF-43: create atom TextField

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 
+import { TextField } from "@mui/material"
+import TextFieldAtom from "./components/atoms/TextFieldAtom"
+
 function App() {
   return (
     <>
-      <div className="App">Hola mundo!</div>
-      <p>Chao mundo!</p>
+      <TextFieldAtom 
+        color={""}
+        id={"standard-basic"}
+        label={"Example Text"}
+        type={"text"}
+        placeholder={"Example Text"} 
+        variant={"standard"}      ></TextFieldAtom>
     </>
+    
   )
 }
 
