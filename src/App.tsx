c987bcd620b918bfbd1b6a3e07758849b19766b8
Damoz1606 <panchitodmun@gmail.com>
2023-01-18 07:32:44 -0500

    Remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,3 @@
-import Code from "@mui/icons-material/Code"
-import { ColorPalette } from "./style/ColorPalette"
-import { ButtonStyle } from "./style/ButtonStyle"
-import { SizeButton } from "./components/atoms/SizeButton"
-
-
 function App() {
   return (<></>
   )
--- src/components/atoms/SizeButton.tsx
+++ src/components/atoms/SizeButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Button, Grid, styled } from '@mui/material';
+import { Box, Button, Grid } from '@mui/material';
 import { ButtonStyle } from '../../style/ButtonStyle';
 
 interface Props {
