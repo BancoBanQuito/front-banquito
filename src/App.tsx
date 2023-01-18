201d515fe3319a453aa764fa525fe5a0d92d6836
Damoz1606 <panchitodmun@gmail.com>
2023-01-18 07:31:09 -0500

    AF-41: Eliminar used imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import { Code, Abc } from "@mui/icons-material";
-import ButtonIcon from "./components/atoms/ButtonIcon";
-import { ColorPalette } from "./style/ColorPalette";
-
 function App() {
 
   return (<></>)
