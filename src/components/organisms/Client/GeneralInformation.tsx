9ed48e999f2c4c47cd26a2fa1e580b77f21d103f
AlexGermancw <agchicaiza2@espe.edu.ec>
2023-01-23 16:42:18 -0500

    Add UI

--- src/components/organisms/Client/GeneralInformation.tsx
+++ src/components/organisms/Client/GeneralInformation.tsx
@@ -1,5 +1,5 @@
-import { Button } from '@mui/material';
-import React from 'react';
+import { Button, TextField } from '@mui/material';
+import React, { useState } from 'react';
 import { ButtonStyle } from '../../../style/ButtonStyle';
 import { Dropdown } from '../../atoms/Dropdown';
 import { SizeButton } from '../../atoms/SizeButton';
@@ -13,71 +13,31 @@ export const GeneralInformation = () => {
   const handleChange = (value) => {
     console.log(value);
   }
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email);
+  };
+
   return (
-    <div style={{ display: 'flex' }}>
-      <div style={{ width: '50%' }}>
-      <SizeButton
-        text="Datos Generales"
-        style={ButtonStyle.BIG}
-        palette={{ backgroundColor: "#1D3557", accent: "#F1FAEE" }}
+    <form onSubmit={handleSubmit}>
+      <TextField
+        label="Name"
+        variant="outlined"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
       />
-      </div>
-      <div>
-        <div>
-          <label>
-            Tipo de Identificación  
-          </label>
-          <br />
-          <label>
-            Identificación
-          </label>
-          <br />
-          <label>
-          Apellido y Nombre
-          </label>
-          <br />
-          <label>
-          Estado
-          </label>
-          <br />
-          <label>
-          Nocionalidad
-          </label>
-          <br />
-          <label>
-          Segmento
-          </label>
-          <br />
-          <label>
-          Correo Electronico  
-          </label>
-          <br />
-          <label>
-          Tipo de Cliente
-          </label>
-        </div>
-      </div>
-      <div style={{ width: '50%' }}>
-        <div>
-          <Dropdown
-            label="Select an option"
-            items={items}
-            width={200}
-            height={40}
-            selectedTextColor="#4B4B4B"
-            onChange={handleChange}
-            inputLabelColor="#4B4B4B"
-            inputFocusedLabelColor="#4B4B4B"
-          />
-          <label>
-            <input type="text" name="name" />
-          </label>
-          <br />
-          <label>
-            <input type="text" name="email" />
-          </label>
-        </div>
-      </div>
-    </div>
+      <TextField
+        label="Email"
+        variant="outlined"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Button variant="contained" color="primary" type="submit">
+        Submit
+      </Button>
+    </form>
   );
 }
