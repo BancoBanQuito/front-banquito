c1546bc561f1b2f0cce983aa7abfacee1c28fa62
Carvajal Patiño Luis Enrique <44406616+carvajalluisweb@users.noreply.github.com>
2023-01-23 18:17:52 -0500

    Sb 43 crear tabs de navegaci n para feriados (#51)
    * tabs para feriados
    
    * formularios de creacion
    
    * Holiday DONE
    
    Co-authored-by: Jorge Luis Galarza Cruz <44406615+jlgalarza3@users.noreply.github.com>
    Co-authored-by: Jorge Galarza <jorge2000cruz@gmail.com>

--- src/pages/UserPages/Holidays/HolidayUser.tsx
+++ src/pages/UserPages/Holidays/HolidayUser.tsx
@@ -1,49 +1,47 @@
-import React from 'react';
-import { Box, Container } from '@mui/material';
-import { useState } from 'react';
-
-import { HolidayTabs } from '../../../components/organisms/Holiday/HolidayTabs';
-import Branch from '../../ClientPages/Branches/Branch';
-import CreateBranch from '../../../components/organisms/Branch/CreateBranch';
-
-
+import React from "react";
+import { Box, Container } from "@mui/material";
+import { useState } from "react";
+import { HolidayTabs } from "../../../components/organisms/Holiday/HolidayTabs";
+import Holiday from "../../ClientPages/Holiday/Holiday";
+import CreateHolidayD from "../../../components/organisms/Holiday/CreateHoliday";
+import CreateHolidayY from "../../../components/organisms/Holiday/CreateHolidayYear";
+import Branch from "../../ClientPages/Branches/Branch";
+import CreateBranch from "../../../components/organisms/Branch/CreateBranch";
 
 const HolidayUser: React.FC = () => {
+  const [tabValue, setTabValue] = useState("Ver");
 
-    const [tabValue, setTabValue] = useState("Ver");
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: string) => {
+    setTabValue(newValue);
+  };
 
-    const handleChange = (event: React.ChangeEvent<{}>, newValue: string) => {
-        setTabValue(newValue);
-    };
-
-    return (
-        <Box sx={boxStyles}>
-            <HolidayTabs tabValue={tabValue} handleChange={handleChange} />
-            <Box sx={{ width: "80%" }}>
-                <Container sx={childStyles}>
-                    {tabValue === "Ver" }
-                </Container>
-                {tabValue === "Crear por Año" }
-                {tabValue === "Crear Por Dia" }
-                
-            </Box>
-        </Box>
-    );
+  return (
+    <Box sx={boxStyles}>
+      <HolidayTabs tabValue={tabValue} handleChange={handleChange} />
+      <Box sx={{ width: "80%" }}>
+        <Container sx={childStyles}>
+          {tabValue === "Ver" && <Holiday />}
+        </Container>
+        {tabValue === "CrearA" && <CreateHolidayY />}
+        {tabValue === "CrearD" && <CreateHolidayD />}
+      </Box>
+    </Box>
+  );
 };
 
 export default HolidayUser;
 
 const boxStyles = () => ({
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "flex-start",
-    marginTop: "200px"
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
+  alignItems: "flex-start",
+  marginTop: "200px",
 });
 
 const childStyles = () => ({
-    position: "relative",
-    width: "100%",
-    height: "100%",
-    marginTop: "-190px"
+  position: "relative",
+  width: "100%",
+  height: "100%",
+  marginTop: "-190px",
 });
