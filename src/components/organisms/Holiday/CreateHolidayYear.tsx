1158432ace870ff112bb941bc4e2e635b2a6fc01
Jorge Galarza <jorge2000cruz@gmail.com>
2023-01-23 18:13:47 -0500

    Holiday DONE

--- src/components/organisms/Holiday/CreateHolidayYear.tsx
+++ src/components/organisms/Holiday/CreateHolidayYear.tsx
@@ -1,122 +1,106 @@
-import React, { useState, useEffect } from 'react'
-import { Container, FormLabel, TextField, Typography } from '@mui/material';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { Dayjs } from 'dayjs';
+import React, { useState } from "react";
+import { Container, FormLabel, TextField, Typography } from "@mui/material";
+import LoadingButton from "@mui/lab/LoadingButton";
 
-
-import Button from '@mui/material/Button';
-
+import Button from "@mui/material/Button";
 
 const CreateHolidayYear: React.FC = () => {
+  const [date, setDate] = useState<string | null>("");
+  const [isLoading, setIsLoading] = useState(false);
 
-
-
-    const [date, setDate] = useState<Dayjs | null>(null);
-
-
-
-
-
-    const handleSubmit = async (event: React.FormEvent) => {
-        event.preventDefault()
-        try {
-
-            const stringDate = date?.format('yyyy-MM-dd')
-
-
-            const response = await fetch('http://localhost:8081/api/holiday', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json', "Access-Control-Allow-Origin": "*" },
-                body: JSON.stringify({
-
-                    date: date,
-
-
-
-                })
-            })
-            if (!response.ok) {
-                throw new Error(response.statusText)
-            }
-            alert("Creada con éxito")
-        } catch (error) {
-            console.error(error)
+  const handleSubmit = async () => {
+    if (!date) return alert("Todos los campos son obligatorios");
+    setIsLoading(true);
+    try {
+      const response = await fetch(
+        `http://localhost:8081/api/holiday/${date}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+          },
         }
+      );
+      if (response.ok) {
+        setIsLoading(false);
+        alert("Creada con éxito");
+      } else {
+        setIsLoading(false);
+        throw new Error(response.statusText);
+      }
+    } catch (error) {
+      setIsLoading(false);
+      console.error(error);
     }
+  };
 
-    return (
-        <>
-            <Container sx={containertTitleStyles}>
-                <Typography variant="h4" align="center">
-                    Crear Feriado
-                </Typography>
-            </Container>
+  return (
+    <>
+      <Container sx={containertTitleStyles}>
+        <Typography variant="h4" align="center">
+          Crear Feriado
+        </Typography>
+      </Container>
+      <Container sx={containerFormLabelStyles}></Container>
+      <Container sx={containerTextFieldStyles}>
+        <FormLabel sx={formLabelStyles}>Fecha:</FormLabel>
+        <TextField
+          variant="standard"
+          sx={formLabelStyles}
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
+      </Container>
 
-            
-            
-            
-            <Container sx={containerFormLabelStyles}>
-
-            </Container>
-            <Container sx={containerTextFieldStyles}>
-                <FormLabel sx={formLabelStyles}>Fecha:</FormLabel>
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <DatePicker
-
-                        value={date}
-                        onChange={(time) => {
-                            setDate(time);
-                        }}
-                        renderInput={(params) => <TextField {...params} variant="standard" sx={formLabelStyles} />}
-                    />
-                </LocalizationProvider>
-
-            </Container>
-
-            <Container sx={containerTextFieldStyles}>
-                <Button onClick={handleSubmit} sx={buttonStyles}>Crear</Button>
-            </Container>
-        </>
-    )
-}
+      <Container sx={containerTextFieldStyles}>
+        <LoadingButton
+          sx={buttonStyles}
+          onClick={handleSubmit}
+          loading={isLoading}
+        >
+          Crear
+        </LoadingButton>
+      </Container>
+    </>
+  );
+};
 
 export default CreateHolidayYear;
 
 const containerStyles = () => ({
-    display: 'flex',
-    justifyContent: 'flex-start',
+  display: "flex",
+  justifyContent: "flex-start",
 });
 
 const containertTitleStyles = () => ({
-    textAlign: 'center',
-    marginTop: '70px',
-    marginBottom: '20px'
+  textAlign: "center",
+  marginTop: "70px",
+  marginBottom: "20px",
 });
 
 const containerTextFieldStyles = () => ({
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    textAlign: 'center',
-    marginTop: '50px',
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  textAlign: "center",
+  marginTop: "50px",
 });
 
 const containerFormLabelStyles = () => ({
-    marginTop: '50px',
-    marginLeft: '280px'
+  marginTop: "50px",
+  marginLeft: "280px",
 });
 
 const formLabelStyles = () => ({
-    marginRight: '10px',
+  marginRight: "10px",
 });
 
 const buttonStyles = () => ({
-    background: '#1D3557',
-    color: 'white',
-    ':hover': {
-        background: '#1D3557',
-        color: 'white'
-    }
+  background: "#1D3557",
+  color: "white",
+  ":hover": {
+    background: "#1D3557",
+    color: "white",
+  },
 });
--- src/pages/ClientPages/Holiday/Holiday.tsx
+++ src/pages/ClientPages/Holiday/Holiday.tsx
@@ -1,83 +1,67 @@
-import React from 'react';
-import { Container, TextField, Typography } from '@mui/material';
-import { useState, useEffect } from 'react';
+import React from "react";
+import { Container, TextField, Typography } from "@mui/material";
+import { useState, useEffect } from "react";
 
-import TableMolecule from '../../../components/molecules/TableMolecule';
-import { IHoliday } from '../../../components/organisms/Holiday/Types';
-
+import TableMolecule from "../../../components/molecules/TableMolecule";
+import { IHoliday } from "../../../components/organisms/Holiday/Types";
 
 const Holiday: React.FC = () => {
+  const [holidays, setBranches] = useState<IHoliday[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const filteredHolidays = holidays.filter((holiday) =>
+    holiday.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
-    const [holidays, setBranches] = useState<IHoliday[]>([]);
-    const [searchTerm, setSearchTerm] = useState('');
-    const filteredHolidays = holidays.filter((holiday) =>
-        holiday.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
+  useEffect(() => {
+    fetch("http://localhost:8081/api/holiday")
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then((data) => setBranches(data))
+      .catch((error) => console.log(error));
+  }, []);
 
-    useEffect(() => {
-        fetch('http://localhost:8081/api/holiday')
-            .then((response) => {
-                if (!response.ok) {
-                    throw Error(response.statusText);
-                }
-                return response.json();
-            })
-            .then((data) => setBranches(data))
-            .catch((error) => console.log(error));
-    }, []);
+  const headers = [<>Fecha</>, <>Codigo</>, <>Nombre</>, <>Tipo</>];
 
-    const headers = [
-        <>Fecha</>,
-        <>Codigo</>,
-        <>Nombre</>,
-        <>Tipo</>,
-        
-    ];
+  const rows = filteredHolidays.map((holiday) => [
+    <>{holiday.date}</>,
+    <>{holiday.code}</>,
+    <>{holiday.name}</>,
+    <>{holiday.type}</>,
+  ]);
 
-    const rows = filteredHolidays.map((holiday) => [
-        <>{holiday.date}</>,
-        <>{holiday.code}</>,
-        <>{holiday.name}</>,
-        <>{holiday.type}</>,
-        
-    ]);
-
-    return (
-        <Container style={containerStyle}>
-            <Container style={{ textAlign: 'center' }}>
-                <Typography variant="h4" align="center">
-                    Feriados
-                </Typography>
-            </Container>
-            <Container style={searchBarStyle}>
-                <TextField
-                    label="Buscar"
-                    value={searchTerm}
-                    onChange={(event) => setSearchTerm(event.target.value)}
-                />
-            </Container>
-            <Container style={containerTableStyle}>
-                <TableMolecule headers={headers} rows={rows} />
-            </Container>
-        </Container>
-    );
+  return (
+    <Container style={containerStyle}>
+      <Container style={{ textAlign: "center" }}>
+        <Typography variant="h4" align="center">
+          Feriados
+        </Typography>
+      </Container>
+      <Container style={containerTableStyle}>
+        <TableMolecule headers={headers} rows={rows} />
+      </Container>
+    </Container>
+  );
 };
 
 export default Holiday;
 
 const containerStyle = {
-    display: 'flex',
-    'flex-direction': 'column',
-    'align-items': 'flex-start',
-    marginTop: '100px'
+  display: "flex",
+  "flex-direction": "column",
+  "align-items": "flex-start",
+  marginTop: "100px",
 };
 
 const containerTableStyle = {
-    marginTop: '10px'
+  marginTop: "10px",
 };
 
 const searchBarStyle = {
-    display: 'flex',
-    'flex-direction': 'column',
-    'align-items': 'flex-start'
+  display: "flex",
+  "flex-direction": "column",
+  "align-items": "flex-start",
 };
