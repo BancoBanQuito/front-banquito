f2c9415161c255ec12d39a3ea6412cd276e2c519
Damoz1606 <panchitodmun@gmail.com>
2023-01-23 19:11:17 -0500

    Remove repeated variable

--- src/components/organisms/AccountSignature/AccountSignatureEditForm.tsx
+++ src/components/organisms/AccountSignature/AccountSignatureEditForm.tsx
@@ -72,8 +72,6 @@ const AccountSignatureEditForm = (props: AccountSignatureEditFormProps) => {
                     action={handleChange}
                     value={signature.identification}
                     placeholder="Ingrese el numero de cedula"
-                    action={() => alert("")}
-                    value=""
                 />
             </div>
             <div style={{ margin: '0.25rem', width: '100%' }}>
