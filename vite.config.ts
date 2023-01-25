5056d8d19d17c8d4392047dd6bbb2d50db1161b7
Damoz1606 <panchitodmun@gmail.com>
2023-01-25 18:48:36 -0500

    Absolute paths

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
-    alias: { '@': path.resolve(__dirname, './src') },
+    alias: { '@': path.resolve(__dirname, 'src'), },
   },
   plugins: [react()],
 })
