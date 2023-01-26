f17699dd802dfd9c42072ce7369cc64b49222e13
Damoz1606 <panchitodmun@gmail.com>
2023-01-26 08:25:01 -0500

    Modify vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
-    alias: { '@': path.resolve(__dirname, 'src'), },
+    alias: [{
+      find: "@",
+      replacement: path.resolve(__dirname, "src")
+    }],
   },
   plugins: [react()],
 })
