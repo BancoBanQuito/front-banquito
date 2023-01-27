9f37ab8a39583f085677d560fb764ac94cf6849e
Japicado <japicado@espe.edu.ec>
2023-01-27 17:00:03 -0500

    feat: :art: info page complete

--- src/pages/UserPages/SearchClientData/SearchClientDataForm.tsx
+++ src/pages/UserPages/SearchClientData/SearchClientDataForm.tsx
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Container, FormLabel, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Container,
+  FormLabel,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
 import Button from "@mui/material/Button";
 
 const urlCloud =
@@ -17,6 +24,7 @@ const SearchClientDataForm: React.FC = () => {
     localStorage.getItem("typeIdentification")
   );
   const [email, setEmail] = useState("");
+  const [fullName, setFullname] = useState("");
   const [birthDate, setBirthDate] = useState("");
   const [genero, setGenero] = useState("");
   const [carrera, setCarrera] = useState("");
@@ -26,6 +34,7 @@ const SearchClientDataForm: React.FC = () => {
   const [telefono, setTelefono] = useState("");
   const [direccion, setDireccion] = useState("");
   const [segmento, setSegmento] = useState("");
+  const [status, setStatus] = useState("");
 
   const formatDate = (date: Date) => {
     return date.toLocaleDateString("es-ES", {
@@ -42,6 +51,7 @@ const SearchClientDataForm: React.FC = () => {
       );
       const data = await response.json();
       setEmail(data.email);
+      setFullname(data.fullName);
       setBirthDate(formatDate(new Date(data.birthDate)));
       setCarrera(data.career);
       setLugarTrabajo(data.companyName);
@@ -51,6 +61,7 @@ const SearchClientDataForm: React.FC = () => {
       setSegmento("VIP");
       setGenero(data.gender);
       setEstadoCivil(data.maritalStatus);
+      setStatus(data.status);
     } catch (error) {
       console.error(error);
     }
@@ -64,109 +75,122 @@ const SearchClientDataForm: React.FC = () => {
     <>
       <Container sx={containertTitleStyles}>
         <Typography variant="h4" align="center">
-          Información Cliente
+          Informacion Cliente
         </Typography>
       </Container>
-      {/* Label Input*/}
-      <Container sx={containerTextFieldStyles}>
-        <FormLabel sx={formLabelStyles}>Email:</FormLabel>
-        <TextField
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-          variant="standard"
-          disabled={isAvailable}
-        />
-      </Container>
-      <Container sx={containerTextFieldStyles}>
-        <FormLabel sx={formLabelStyles}>Fecha de Nacimiento:</FormLabel>
-        <TextField
-          value={birthDate}
-          onChange={(event) => setBirthDate(event.target.value)}
-          variant="standard"
-          disabled={isAvailable}
-        />
-      </Container>
-      <Container sx={containerTextFieldStyles}>
-        <FormLabel sx={formLabelStyles}>Carrera:</FormLabel>
-        <TextField
-          value={carrera}
-          onChange={(event) => setCarrera(event.target.value)}
-          variant="standard"
-          disabled={isAvailable}
-        />
-      </Container>
-      <Container sx={containerTextFieldStyles}>
-        <FormLabel sx={formLabelStyles}>Lugar de Trabajo:</FormLabel>
-        <TextField
-          value={lugarTrabajo}
-          onChange={(event) => setLugarTrabajo(event.target.value)}
-          variant="standard"
-          disabled={isAvailable}
-        />
-      </Container>
-      <Container sx={containerTextFieldStyles}>
-        <FormLabel sx={formLabelStyles}>Referencia:</FormLabel>
-        <TextField
-          value={referencia}
-          onChange={(event) => setReferencia(event.target.value)}
-          variant="standard"
-          disabled={isAvailable}
-        />
-      </Container>
-      <Container sx={containerTextFieldStyles}>
-        <FormLabel sx={formLabelStyles}>Telefono:</FormLabel>
-        <TextField
-          value={telefono}
-          onChange={(event) => setTelefono(event.target.value)}
-          variant="standard"
-          disabled={isAvailable}
-        />
-      </Container>
+      <Grid container>
+        <Grid item xs={7}>
+          {/* Label Input*/}
+          <Container sx={containerTextFieldStyles}>
+            <FormLabel sx={formLabelStyles}>Email:</FormLabel>
+            <TextField
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              variant="standard"
+              disabled={isAvailable}
+            />
+          </Container>
+          <Container sx={containerTextFieldStyles}>
+            <FormLabel sx={formLabelStyles}>Fecha de Nacimiento:</FormLabel>
+            <TextField
+              value={birthDate}
+              onChange={(event) => setBirthDate(event.target.value)}
+              variant="standard"
+              disabled={isAvailable}
+            />
+          </Container>
+          <Container sx={containerTextFieldStyles}>
+            <FormLabel sx={formLabelStyles}>Carrera:</FormLabel>
+            <TextField
+              value={carrera}
+              onChange={(event) => setCarrera(event.target.value)}
+              variant="standard"
+              disabled={isAvailable}
+            />
+          </Container>
+          <Container sx={containerTextFieldStyles}>
+            <FormLabel sx={formLabelStyles}>Lugar de Trabajo:</FormLabel>
+            <TextField
+              value={lugarTrabajo}
+              onChange={(event) => setLugarTrabajo(event.target.value)}
+              variant="standard"
+              disabled={isAvailable}
+            />
+          </Container>
+          <Container sx={containerTextFieldStyles}>
+            <FormLabel sx={formLabelStyles}>Referencia:</FormLabel>
+            <TextField
+              value={referencia}
+              onChange={(event) => setReferencia(event.target.value)}
+              variant="standard"
+              disabled={isAvailable}
+            />
+          </Container>
+          <Container sx={containerTextFieldStyles}>
+            <FormLabel sx={formLabelStyles}>Telefono:</FormLabel>
+            <TextField
+              value={telefono}
+              onChange={(event) => setTelefono(event.target.value)}
+              variant="standard"
+              disabled={isAvailable}
+            />
+          </Container>
+        </Grid>
 
-      <Container sx={containerTextFieldStyles}>
-        <FormLabel sx={formLabelStyles}>Direccion:</FormLabel>
-        <TextField
-          value={direccion}
-          onChange={(event) => setDireccion(event.target.value)}
-          variant="standard"
-          disabled={isAvailable}
-        />
-      </Container>
-      <Container sx={containerTextFieldStyles}>
-        <FormLabel sx={formLabelStyles}>Segmento:</FormLabel>
-        <TextField
-          value={segmento}
-          onChange={(event) => setSegmento(event.target.value)}
-          variant="standard"
-          disabled={isAvailable}
-        />
-      </Container>
+        <Grid item xs={3}>
+          <Container>
+            <Box sx={boxStile}>
+              <p>{fullName}</p>
+              <p>Identificación: {idCliente}</p>
+              <p>Tipo de Itentificación: {typeIdentification}</p>
+              <p>Estado: {status}</p>
+            </Box>
+          </Container>
+          <Container sx={containerTextFieldStyles}>
+            <FormLabel sx={formLabelStyles}>Direccion:</FormLabel>
+            <TextField
+              value={direccion}
+              onChange={(event) => setDireccion(event.target.value)}
+              variant="standard"
+              disabled={isAvailable}
+            />
+          </Container>
+          <Container sx={containerTextFieldStyles}>
+            <FormLabel sx={formLabelStyles}>Segmento:</FormLabel>
+            <TextField
+              value={segmento}
+              onChange={(event) => setSegmento(event.target.value)}
+              variant="standard"
+              disabled={isAvailable}
+            />
+          </Container>
 
-      <Container sx={containerTextFieldStyles}>
-        <FormLabel sx={formLabelStyles}>Estado Civil:</FormLabel>
-        <TextField
-          value={estadoCivil}
-          onChange={(event) => setSegmento(event.target.value)}
-          variant="standard"
-          disabled={isAvailable}
-        />
-      </Container>
-      <Container sx={containerTextFieldStyles}>
-        <FormLabel sx={formLabelStyles}>Genero:</FormLabel>
-        <TextField
-          value={genero}
-          onChange={(event) => setSegmento(event.target.value)}
-          variant="standard"
-          disabled={isAvailable}
-        />
-      </Container>
-
-      {/* Boton*/}
-      <Container sx={containerTextFieldStyles}>
-        <Button onClick={() => {}} sx={buttonStyles}>
-          Pagina Principal
-        </Button>
-      </Container>
+          <Container sx={containerTextFieldStyles}>
+            <FormLabel sx={formLabelStyles}>Estado Civil:</FormLabel>
+            <TextField
+              value={estadoCivil}
+              onChange={(event) => setSegmento(event.target.value)}
+              variant="standard"
+              disabled={isAvailable}
+            />
+          </Container>
+          <Container sx={containerTextFieldStyles}>
+            <FormLabel sx={formLabelStyles}>Genero:</FormLabel>
+            <TextField
+              value={genero}
+              onChange={(event) => setSegmento(event.target.value)}
+              variant="standard"
+              disabled={isAvailable}
+            />
+          </Container>
+        </Grid>
+        {/* Boton*/}
+        <Container sx={containerTextFieldStyles}>
+          <Button onClick={() => {}} sx={buttonStyles}>
+            Pagina Principal
+          </Button>
+        </Container>
+      </Grid>
     </>
   );
 };
@@ -199,4 +223,14 @@ const buttonStyles = () => ({
     background: "#E63946",
     color: "white",
   },
+});
+const boxStile = () => ({
+  textAlign: "left",
+  color: "#F1FAEE",
+  marginTop: "70px",
+  marginBottom: "20px",
+  background: "#1D3557",
+  padding: "20px",
+  borderRadius: "10px",
+  fontSize: "20px",
 });
