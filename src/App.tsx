b7583c0da57aaa816637e616003cea1432140056
Ismael Sebastián Andrango Peralbo <51093008+SebastianAndrango@users.noreply.github.com>
2023-01-26 23:53:09 -0500

    AF-71: Conect API with UI (#73)
    * AF-71: Conect API with UI
    
    * Removed localhost
    
    Co-authored-by: Francisco David Muñoz Armas <51092954+FranciscoDavidMunozArmas@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -35,6 +35,7 @@ import { UpdateBankEntity } from './components/organisms/BankEntity/UpdateBankEn
 import { Product } from "./pages/ProductPages/Product";
 import { ProductType } from "./pages/ProductPages/ProductType";
 import CreateRequestService from './pages/CreateRequestService';
+import PaymentCheckbook from "./pages/ClientPages/Account/CheckbookPayment";
 
 const App = () => {
 
@@ -154,6 +155,10 @@ const App = () => {
     {
       path: "transaccion",
       element: <TransferUser />,
+    },
+    {
+      path: "pagos/cheque",
+      element: <PaymentCheckbook />,
     },
     {
       path: "signup",
