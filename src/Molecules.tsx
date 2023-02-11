5ee3bc27e5fc4ffed4919f3f4922d4117894c3ec
Damoz1606 <panchitodmun@gmail.com>
2023-02-11 12:29:34 -0500

    Snack bar

--- src/Molecules.tsx
+++ src/Molecules.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ATMButtonContainerMolecule from './components/molecules/ATMButtonContainerMolecule'
 import ATMButtonAtom from './components/atoms/ATMButtonAtom'
 import { Code } from '@mui/icons-material'
@@ -8,8 +8,13 @@ import ATMReturnHome from './pages/ATMPages/ATMReturnHome'
 import AccountAvailableBalance from './pages/ATMPages/Account/AccountAvailableBalance'
 import DepositAtm from './pages/ATMPages/Transaction/DepositAtm'
 import WithdrawAtm from './pages/ATMPages/Transaction/WithdrawAtm'
+import SnackBarMolecule from './components/molecules/SnackBarMolecule'
+import Button from '@mui/material/Button/Button'
 
 const Molecules = () => {
+
+    const [open, setopen] = useState(false)
+
     return (
         <div style={{
             display: 'flex',
@@ -17,7 +22,13 @@ const Molecules = () => {
             justifyContent: 'center',
             alignContent: 'center'
         }}>
-            <WithdrawAtm />
+            <Button onClick={() => setopen(true)}>Click me</Button>
+            <SnackBarMolecule
+            severity='warning'
+                open={open}
+                onClose={() => setopen(false)}
+                message={'Sample'}
+                title='Info' />
         </div>
     )
 }
