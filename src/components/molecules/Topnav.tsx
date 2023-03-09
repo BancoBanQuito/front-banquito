5caa219cfba547c1f3695e8d207839a4f485db35
listerineh <jsalvarez4@espe.edu.ec>
2023-03-08 19:26:20 -0500

    DEPLOY COMMIT

--- src/components/molecules/Topnav.tsx
+++ src/components/molecules/Topnav.tsx
@@ -7,7 +7,6 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import { Link, useNavigate } from 'react-router-dom';
 import { Button } from '@mui/material';
@@ -44,39 +43,6 @@ const Topnav = ({ to }: TopnavProps) => {
     setAnchorEl(null);
   };
 
-  /* const getBankEntity = async () => {
-    const url = `${EnvManager.SETTINGS_URL}/api/bank-entity`;
-    const options = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-      },
-    };
-
-    try {
-      setActivateSpinner(true);
-      const response = await fetch(url, options);
-      if (response.ok) {
-        const data = await response.json();
-        setBankEntity(data[0].name);
-        setActivateSpinner(false);
-        return data;
-      } else {
-        setActivateSpinner(false);
-        throw new Error(response.statusText);
-      }
-    } catch (error: any) {
-      setActivateSpinner(false);
-      if (error.message === "Bad Request") {
-        alert("Error: 400 Bad Request");
-      } else if (error.message === "Internal Server Error") {
-        alert("Error en el servidor, intente más tarde");
-      } else {
-        alert("Error desconocido, intente más tarde");
-      }
-    }
-  }; */
 
   const handleLogout = () => {
     logout();
@@ -86,11 +52,6 @@ const Topnav = ({ to }: TopnavProps) => {
     user.isLogged = false;
     window.location.reload();
   };
-
-  /* useEffect(() => {
-    getBankEntity();
-  }, []
-  ); */
 
   return (
     <>
